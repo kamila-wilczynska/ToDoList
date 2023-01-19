965fa9ba59f7fbd974c4d17de0323afc131d431d
unknown <kamila.wilczynska92@gmail.com>
2023-01-19 21:24:30 +0000

    Display 2 lists

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 //import Container from "../Container/Container";
 import Hero from "../Hero/Hero";
 import SearchForm from "../SearchForm/SearchForm";
-import List from "../List/List";
+import Lists from '../Lists/Lists';
 
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
 		<>
 			<Hero />
 			<SearchForm />
-			<List />
+			<Lists />
 		
 		</>
 	);
