6e96ab6bee44a3c1763a9c37835778719f9b8f76
unknown <kamila.wilczynska92@gmail.com>
2023-01-21 16:55:38 +0000

    fix add column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,28 +4,34 @@ import ColumnForm from './../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
 import { getListById } from '../../redux/store';
 import { getColumnsByList } from '../../redux/store';
+import { useParams } from 'react-router';
 
-const List = () => {
-	const columns = useSelector((state) => getColumnsByList(state, 1));
+const List = (props) => {
+	//useParams zwraca nam po prostu obiekt z parametrami adresu
+	const { listId } = useParams();
+	const columns = useSelector((state) => getColumnsByList(state, listId));
 	//display just with the id 1 
-	const listData = useSelector((state) => getListById(state, 1));
+	const listData = useSelector((state) => getListById(state, listId));
 
 	return (
+		<>
 		<div className={styles.list}>
-			<header className={styles.header}>
-				<h2 className={styles.title}>
-				{listData.title}
-				</h2>
-			</header>
-			<p className={styles.description}>Interesting things I want to check out</p>
-			<section className={styles.columns}>
-				{columns.map((column) => (
-					<Column key={column.id} {...column} />
-				))}
-			</section>
-			<ColumnForm />
+		  <header className={styles.header}>
+			<h2 className={styles.title}>
+			  {listData.title}
+			</h2>
+		  </header>
+		  <p className={styles.description}>{listData.description}</p>
+		  <section className={styles.columns}>
+			{columns.map((column) => (
+			  <Column key={column.id} {...column} />
+			))}
+		  </section>
+		  
+		  <ColumnForm listId={listId} />
 		</div>
-	);
+		</>
+	  );
 };
 
 export default List;
